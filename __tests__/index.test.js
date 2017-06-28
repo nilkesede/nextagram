760055a92cd35805d6157cce9d8ee30c2b38c331
nilkesede <nil@ksde.pw>
2017-06-28 14:49:37 -0300

    Add jest and enzyme test suite

---
+++ __tests__/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Index from '../pages/index.js';
+
+describe('on Index', () => {
+  it('shows the "Nextagram" title', () => {
+    const index = shallow(<Index/>);
+
+    expect(index.find('h1').text()).toEqual('Nextagram');
+  });
+});
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import Head from '../components/head';
 
 export default class Index extends Component {
-  render() {
-    const title = 'Nextagram';
+  title = 'Nextagram';
 
+  render() {
     return (
       <div>
-        <Head title={title}/>
-        <h1>{title}</h1>
+        <Head title={this.title}/>
+        <h1>{this.title}</h1>
       </div>
     );
   }
